0f9c4bbfe8f112250b53c20c0467f221e3b628a4
Jason Kirby <Jason Kirby>
2016-11-06 10:10:02 -0700

    Have partial authentication working. Works currently with Windows password. Once Active Directory configuration is sent, I need to integrate it with AD

--- VIP_Parking/VIP_Parking/Controllers/HomeController.cs
+++ VIP_Parking/VIP_Parking/Controllers/HomeController.cs
@@ -8,38 +8,11 @@ namespace VIP_Parking.Controllers
 {
     public class HomeController : Controller
     {
-        [AllowAnonymous]
+        [Authorize]
         public ActionResult Index()
         {
             ViewBag.Message = "";
 
             return View();
-        }
-        [HttpPost]
-        public ActionResult Index(LoginModel model, string returnUrl)
-        {
-            if (ModelState.IsValid)
-            {
-                UserManager UM = new UserManager();
-                string password = UM.GetUserPassword(ULV.LoginName);
-
-                if (string.IsNullOrEmpty(password))
-                    ModelState.AddModelError("", "The user login or password provided is incorrect.");
-                else {
-                    if (ULV.Password.Equals(password))
-                    {
-                        FormsAuthentication.SetAuthCookie(ULV.LoginName, false);
-                        return RedirectToAction("Welcome", "Home");
-                    }
-                    else {
-                        ModelState.AddModelError("", "The password provided is incorrect.");
-                    }
-                }
-            }
-
-            // If we got this far, something failed, redisplay form  
-            return View(ULV);
-        }
-
-    }
+        }    }
 }
---
+++ VIP_Parking/VIP_Parking/Controllers/LoginController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Web.Mvc;
+using ActiveDirectoryAuthentication.Models;
+using Microsoft.Owin.Security;
+using MyProject;
+
+namespace ActiveDirectoryAuthentication.Controllers
+{
+    public class LoginController : Controller
+    {
+        [AllowAnonymous]
+        public virtual ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated) //If user is already logged in, redirect them to the home page
+                return RedirectToAction("Index", "Home");
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public virtual ActionResult Index(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // usually this will be injected via DI. but creating this manually now for brevity
+            IAuthenticationManager authenticationManager = HttpContext.GetOwinContext().Authentication;
+            var authService = new AdAuthenticationService(authenticationManager);
+
+            var authenticationResult = authService.SignIn(model.Username, model.Password);
+
+            if (authenticationResult.IsSuccess)
+            {
+                // we are in!
+                return RedirectToLocal(returnUrl);
+            }
+
+            ModelState.AddModelError("", authenticationResult.ErrorMessage);
+            return View(model);
+        }
+
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        public virtual ActionResult Logoff()
+        {
+            IAuthenticationManager authenticationManager = HttpContext.GetOwinContext().Authentication;
+            authenticationManager.SignOut(MyAuthentication.ApplicationCookie);
+
+            return RedirectToAction("Index");
+        }
+    }
+
+
+    public class LoginViewModel
+    {
+        [Required, AllowHtml]
+        public string Username { get; set; }
+
+        [Required]
+        [AllowHtml]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
