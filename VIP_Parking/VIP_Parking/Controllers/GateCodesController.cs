95990dcf491b2bfeafdc18095611af2a4fed215c
bigboyd75 <bigboyd75@gmail.com>
2016-12-06 21:38:20 -0800

    Attempts at loading data into SQL from excel

--- VIP_Parking/VIP_Parking/Controllers/GateCodesController.cs
+++ VIP_Parking/VIP_Parking/Controllers/GateCodesController.cs
@@ -3,10 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using System.Web.Mvc;
 using VIP_Parking.Models.Database;
 
